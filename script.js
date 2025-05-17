ccbf611db99339266aed9b8d229b488f1a92aab0
JamesJakkritC <jakkrit.choosawat@gmail.com>
2025-05-17 18:04:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ async function checkIn() {
     return;
   }
 
-  const response = await fetch('https://script.google.com/macros/s/AKfycbxBDUjHfQPgvqgo55mi0H9fKEvq1Bni7sl6ei2G0ai0Y5DV4RhIaSPvRxv1r1bVEMcg/exec', {
+  const response = await fetch('https://script.google.com/macros/s/AKfycbwBkpnW1-J5bO6p2QAxPnIsOrf4WwgIcgmHf8SfOI9U7UwSqs9g7VnPRHg-SPNDykto/exec', {
     method: 'POST',
     body: JSON.stringify({ name }),
     headers: { 'Content-Type': 'application/json' }
