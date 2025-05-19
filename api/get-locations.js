c979de0f046edd12c1bc282488409369b49302d1
JamesJakkritC <jakkrit.choosawat@gmail.com>
2025-05-19 22:10:33 +0700

    Update get-locations.js

--- api/get-locations.js
+++ api/get-locations.js
@@ -5,9 +5,10 @@ const tempFile = path.join('/tmp', 'locations.json');
 
 export default function handler(req, res) {
   try {
-    if (!fs.existsSync(tempFile)) {
-      return res.status(200).json([]); // Return empty array if file not found
-    }
+      if (!fs.existsSync(tempFile)) {
+        console.log("No file found");
+        return res.status(200).json([]); // <- will return empty if file was never created
+      }
 
     const data = fs.readFileSync(tempFile, 'utf-8');
     const locations = JSON.parse(data);
