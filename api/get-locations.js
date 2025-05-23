fe974862599def221cba1e344f90c548ef1c033b
JamesJakkritC <jakkrit.choosawat@gmail.com>
2025-05-23 21:32:25 +0700

    Update get-locations.js

--- api/get-locations.js
+++ api/get-locations.js
@@ -1,15 +1,1 @@
-import fs from 'fs';
-import path from 'path';
 
-export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'locations.json');
-
-  if (!fs.existsSync(filePath)) {
-    return res.status(200).json([]); // return empty array if no file
-  }
-
-  const data = fs.readFileSync(filePath);
-  const locations = JSON.parse(data);
-
-  res.status(200).json(locations);
-}
