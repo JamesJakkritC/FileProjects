cd2e598cb2bf2b51c4f328ff0d29d48e8fd2dc8a
JamesJakkritC <jakkrit.choosawat@gmail.com>
2025-05-24 12:36:29 +0700

    Update scriptAdmin.js

--- scriptAdmin.js
+++ scriptAdmin.js
@@ -1,4 +1,4 @@
-const GOOGLE_SCRIPT_URL = `https://script.google.com/macros/s/AKfycby2bvzG7PzE0y3VnVTjqfPQoKCE4ihuls9hoFdPRUZ_XXeUgpu3gR7PfrrbBwRCGPua/exec`;
+const GOOGLE_SCRIPT_URL = `https://script.google.com/macros/s/AKfycbxOAfEkeoozGTSbEX2vhmHQ-L-nhnXshnsXhWXIZ-m-m539N6gQaz0e6f6Wj09A52lr/exec`;
 const ADMIN_USER = "admin";
 const ADMIN_PASS = "123456";
 
