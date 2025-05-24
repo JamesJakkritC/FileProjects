d1a9f1138198ab0f37ff12dc7925906e5ffc4336
JamesJakkritC <jakkrit.choosawat@gmail.com>
2025-05-24 12:21:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let ALLOWED_LOCATIONS = [];
-
+const GOOGLE_SCRIPT_URL = `https://script.google.com/macros/s/AKfycbylLMsIelEtc3pEPfT3YLhiZwr0ckv1hOwuuL4ft_ZptuLXzoLhFODD7d5Li0uMnHV0/exec`;
 let currentLocationName = "";
 let userLat = 0;
 let userLng = 0;
